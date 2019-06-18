deca66e9c75f210a484b4b5a3520d47b26dce301
YuraTaranov <47536902+YuraTaranov@users.noreply.github.com>
2019-06-18 11:27:24 +0300

    Update bundle.js

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -395,7 +395,7 @@ module.exports = tabs;
 
 function timers() {
     
-    let deadline = '2019-06-13';
+    let deadline = '2019-07-13';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date());
